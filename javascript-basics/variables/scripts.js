9043487c41a118607c2b32bcbedea333326b22c9
SteveMustanki <steve.mustanski@gmail.com>
2018-07-18 16:59:20 -0400

    more working with variables

--- javascript-basics/variables/scripts.js
+++ javascript-basics/variables/scripts.js
@@ -1,3 +1,6 @@
 let visitor = prompt('What is your name?');
 let message = `Hello ${visitor}!  Glad that you came by to visit`;
 document.write(message);
+
+let stringToShout = prompt('What would you like to shout?');
+alert(`${visitor} shouts: "${stringToShout.toUpperCase()}" !`);
