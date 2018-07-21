0f887c13e1731008f82b71caf84337896d2635c1
SteveMustanki <steve.mustanski@gmail.com>
2018-07-20 22:38:50 -0400

    added upper and lower arguments for random number gen and throw error

--- javascript-basics/functions/random.js
+++ javascript-basics/functions/random.js
@@ -1,9 +1,13 @@
-const getRandomNumber = (upperNum) =>  {
-  let randomNumber = Math.floor(Math.random() * upperNum) + 1;
-  return randomNumber;
+const getRandomNumber = (lowerNum, upperNum) =>  {
+  if (isNaN(lowerNum) || isNaN(upperNum)) {
+    throw new Error('one of the inputs is not a number');
+  }
+  return Math.floor(Math.random() * (upperNum - lowerNum + 1)) + lowerNum;
 };
 
-alert(getRandomNumber(4));
+alert(getRandomNumber(1,4));
+alert(getRandomNumber(1, 20));
+alert(getRandomNumber('six', 20));
 
 
 
