2d0f232c7fe4779187b0b09011e578cbc51236c6
SteveMustanki <steve.mustanski@gmail.com>
2018-07-27 16:22:37 -0400

    while loop to generate random number x times

--- javascript-loops-arrays-objects/script.js
+++ javascript-loops-arrays-objects/script.js
@@ -1,3 +1,10 @@
 function randomNumber(upper) {
   return Math.floor( Math.random() * upper ) + 1;
 }
+
+let counter = 0;
+while (counter < 10000) {
+  let randNum = randomNumber(6);
+  document.write(`${randNum} `)
+  counter += 1
+}
