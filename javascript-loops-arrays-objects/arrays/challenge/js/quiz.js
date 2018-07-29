1a1e3d549484094a9a62ad92ed9b3171aab4d5c5
SteveMustanki <steve.mustanski@gmail.com>
2018-07-29 10:26:32 -0400

    added arrays quiz challenge

---
+++ javascript-loops-arrays-objects/arrays/challenge/js/quiz.js
@@ -0,0 +1,29 @@
+
+function print(message) {
+  document.write(message);
+}
+
+// set up questions and answers
+let correct = 0;
+let message = '';
+let quizArray = [
+  ['Who is an awesome developer?', 'Steve'],
+  ['Pinapple on a pizza (y/n)?', 'n'],
+  ['What language is this written in?', 'javascript']
+]
+
+// process array and ask questions
+
+for (let i = 0; i < quizArray.length; i++) {
+  let answer = prompt(quizArray[i][0]);
+
+  // evaluate answers and store correct count
+  if (answer.toLowerCase === quizArray[i][1].toLowerCase) {
+    correct += 1;
+  }
+  
+}
+// print final results
+
+message = `You scored ${correct} correct out of ${quizArray.length} questions`;
+print(message);
