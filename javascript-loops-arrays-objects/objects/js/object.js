ba38c38bd5a1f2333b2c75a1f6114b35b1ac9181
SteveMustanki <steve.mustanski@gmail.com>
2018-07-29 17:19:22 -0400

    access object properties and add them to the page

--- javascript-loops-arrays-objects/objects/js/object.js
+++ javascript-loops-arrays-objects/objects/js/object.js
@@ -4,4 +4,16 @@ let person = {
   age: 35,
   treehouseStudent: true,
   skills: ['JavaScript', 'HTML', 'CSS']
-}
+};
+
+function print(message) {
+  let div = document.getElementById('output');
+  div.innerHTML = message;
+  
+};
+
+let message = `<p>Hello. My name is ${person.name}.</p>`;
+message += `<p>I live in the ${person.country}.</p>`;
+message += `<p>I am ${person.age}.</p>`;
+message += `<p>My skills include ${person.skills.join(', ')}.</p>`;
+print(message);
