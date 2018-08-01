469abe2dc32f74db10384aceb462abd03ece5628
SteveMustanki <steve.mustanski@gmail.com>
2018-08-01 16:10:22 -0400

    selecting multiple elements

--- javascript-dom/app.js
+++ javascript-dom/app.js
@@ -1,9 +1,15 @@
-const myHeading = document.getElementById('myHeading');
-const myButton = document.getElementById('myButton');
-const myTextInput = document.getElementById('myTextInput');
+const myList = document.getElementsByTagName('li');
+const errorNotPurple = document.getElementsByClassName('error-not-purple');
 
 
-myButton.addEventListener('click', () => {
-  myHeading.style.color = myTextInput.value;
-});
+for (let i = 0; i < myList.length; i++) {
+  myList[i].style.color = 'purple';
+  
+}
+
+for (let i = 0; i < errorNotPurple.length; i++) {
+  errorNotPurple[i].style.color = 'red';
+  
+}
+
 
