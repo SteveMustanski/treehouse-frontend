f583d3bbbb4ad3a3b52edcce949f26dcd019b3a2
SteveMustanki <steve.mustanski@gmail.com>
2018-08-03 16:48:45 -0400

    added remove last list item functionality

--- javascript-dom/app2.js
+++ javascript-dom/app2.js
@@ -5,9 +5,11 @@ const descriptionP = document.querySelector('p.description');
 const descriptionButton = document.querySelector('button.description');
 const addItemInput = document.querySelector('input.addItemInput');
 const addItemButton = document.querySelector('button.addItemButton')
+const removeItemButton = document.querySelector('button.removeItemButton')
 
 descriptionButton.addEventListener('click', () => {
   descriptionP.innerHTML = descriptionInput.value + ':';
+  descriptionInput.value = '';
 })
 
 toggle.addEventListener('click', () => {
@@ -26,5 +28,13 @@ addItemButton.addEventListener('click', () => {
   let li = document.createElement('li');
   li.textContent = addItemInput.value;
   ul.appendChild(li);
+  // clear the text input field
   addItemInput.value = '';
+})
+
+removeItemButton.addEventListener('click', () => {
+  let ul = document.getElementsByTagName('ul')[0];
+  // select the last child
+  let lastLi = document.querySelector('li:last-child');
+  ul.removeChild(lastLi);
 })
