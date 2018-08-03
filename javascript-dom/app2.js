ebae7c68800f4117340ee39bfb1bb367cbf42fd1
SteveMustanki <steve.mustanski@gmail.com>
2018-08-02 23:32:09 -0400

    change text and change properties with javascript

--- javascript-dom/app2.js
+++ javascript-dom/app2.js
@@ -1,7 +1,19 @@
+const toggle = document.querySelector('#toggleList');
+const list = document.querySelector('.list');
 const input = document.querySelector('input');
 const p = document.querySelector('p.description');
-const button = document.querySelector('button');
+const button = document.querySelector('button.description');
 
 button.addEventListener('click', () => {
   p.innerHTML = input.value + ':';
+})
+
+toggle.addEventListener('click', () => {
+  if (list.style.display === 'none') {
+    toggle.textContent = 'Hide List'
+    list.style.display = 'block';
+  } else {
+    toggle.textContent = 'Show List';
+    list.style.display = 'none';
+  }
 })
