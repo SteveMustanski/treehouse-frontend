511d532ce9ca9a75556f46e655818e89cc585eca
SteveMustanki <steve.mustanski@gmail.com>
2018-08-10 13:44:46 -0400

    set conent and html on the page using jquery

--- jquery/blog-previewer/js/app.js
+++ jquery/blog-previewer/js/app.js
@@ -3,3 +3,9 @@ $('#flashMessage')
   .fadeIn(1000)
   .delay(3000)
   .slideUp();
+
+  const title = 'My First Blog Post'; 
+  const content = 'This is my <strong>first</strong> post!';
+
+  $('#blogTitlePreview').text(title);
+  $('#blogContentPreview').html(content);
