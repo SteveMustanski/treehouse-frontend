728cce28c46ad35dc80ec26622f7f1b9a81971da
SteveMustanski <steve.mustanski@gmail.com>
2018-08-11 17:09:25 -0400

    use jquery to add the button to the screen in the enent users don't have js running

--- jquery/spoiler/js/app.js
+++ jquery/spoiler/js/app.js
@@ -1,10 +1,14 @@
 
-// hide the spoiler on page load
+// create reveal spoiler button and append to page at end of spoiler class
 
+const $button = $('<button>Reveial Spoiler</button>');
+$('.spoiler').append($button);
+
+
+// hide the spoiler on page load
 $('.spoiler span').hide();
 
 // when the button is pressed show spoiler and hide button
-
 $('.spoiler button').click( ()=> {
   $('.spoiler button').hide();
   $('.spoiler span').show();
