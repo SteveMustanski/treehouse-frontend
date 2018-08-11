99cda8bd1d9ad457ea59229341deed29f2107172
SteveMustanski <steve.mustanski@gmail.com>
2018-08-11 16:39:00 -0400

    added hide spoiler at start and show on button click

--- jquery/spoiler/js/app.js
+++ jquery/spoiler/js/app.js
@@ -1,4 +1,15 @@
 
+// hide the spoiler on page load
+
+$('.spoiler span').hide();
+
+// when the button is pressed show spoiler and hide button
+
+$('.spoiler button').click( ()=> {
+  $('.spoiler button').hide();
+  $('.spoiler span').show();
+});
+
 
 
 
