6f01bc0c20c05f3658eecc634e3da4278a9524a7
SteveMustanski <steve.mustanski@gmail.com>
2018-08-13 22:24:31 -0400

    simple playing with objects

---
+++ objects/pets.js
@@ -0,0 +1,12 @@
+const pepper = {
+  animal: 'dog',
+  age: 8,
+  breed: 'border terrier',
+  bark: () => {
+    console.log('Bark!');
+  }
+}
+
+console.log(pepper.age);
+console.log(pepper.breed);
+pepper.bark();
