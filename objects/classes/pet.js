021c6965077ca401b55c8d1ea36d817a01e70535
SteveMustanski <steve.mustanski@gmail.com>
2018-08-14 23:18:10 -0400

    added speak method to Pet class

--- objects/classes/pet.js
+++ objects/classes/pet.js
@@ -1,12 +1,18 @@
 class Pet {
-  constructor(animal, age, breed) {
+  constructor(animal, age, breed, sound) {
     this.animal = animal;
     this.age = age;
     this.breed = breed;
+    this.sound = sound;
+  }
+
+  speak() {
+    console.log(this.sound);
   }
 }
 
-const ernie = new Pet('dog', 1, 'pug');
-const pepper = new Pet('dog', 8, 'border terrier');
+const ernie = new Pet('dog', 1, 'pug', 'weeez');
+const pepper = new Pet('dog', 8, 'border terrier', 'bark!');
 
-console.log(pepper);
+ernie.speak();
+pepper.speak();
