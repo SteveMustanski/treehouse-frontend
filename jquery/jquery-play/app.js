f1e8906ebc295f43fcb87345dd07f1cfc44e68f0
SteveMustanski <steve.mustanski@gmail.com>
2018-08-30 16:37:49 -0400

    added check for loss

--- jquery/jquery-play/app.js
+++ jquery/jquery-play/app.js
@@ -1,31 +1,40 @@
 
-var numToEqual = Math.floor(Math.random() * 100) + 1;
 
-var blueVal = 4;
-var redVal = 8;
-var greenVal = 1;
-var yellowVal = 3;
-var userTot = 0;
+let numToEqual = 0;
+let blueVal = 4;
+let redVal = 8;
+let greenVal = 1;
+let yellowVal = 3;
+let userTot = 0;
+let losses = 0;
+let wins = 0;
+
 
 writeComputerTotal();
+writeUserTotal(userTot);
 
 $("#blue").on('click', () => {
   writeUserTotal(blueVal);
+  checkResults();
 });
 
 $("#red").on('click', () => {
   writeUserTotal(redVal);
+  checkResults();
 });
 
 $("#green").on('click', () => {
   writeUserTotal(greenVal);
+  checkResults();
 });
 
 $("#yellow").on('click', () => {
   writeUserTotal(yellowVal);
+  checkResults();
 });
 
 function writeComputerTotal() {
+  numToEqual = Math.floor(Math.random() * 100) + 1;
   $("#computerNum").text(`Target Number: ${numToEqual}`);
 }
 
@@ -33,4 +42,29 @@ function writeUserTotal(num) {
   userTot += num;
   $("#userNum").text(`Your total: ${userTot}`);
 }
+
+function resetGame() {
+  writeComputerTotal();
+  userTot = 0;
+  writeUserTotal(userTot);
+}
+
+function checkResults() {
+  //if num >
+  if (userTot > numToEqual) {
+  //alert you lose
+  alert("You just lost the game...");
+  //add 1 to loss and write out
+  losses+=1;
+  //reset num to equal and usertot
+  resetGame();
+  }
+  // if num =
+  // alert you win
+  // add 1 to win
+  // reset num to equal and usertot
+}
+
+
+
 
